f7f00f2af385126c6fa19e626d67911876072836
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-01-02 00:33:19 +0530

    Auth and Add-Course

--- project/models/user.js
+++ project/models/user.js
@@ -1,20 +1,19 @@
 var mongoose = require("mongoose");
 
 var userSchema = new mongoose.Schema({
-	name: String,
-	email: String,
-	image: String,
-	department: String,
-	designation: String,
-	description: String,
-	phone: String,
-	courses: [
-		{
-			type : mongoose.Schema.Types.ObjectId,
-			ref: "Course"
-		}
-	]
+  name: String,
+  email: String,
+  image: String,
+  department: String,
+  designation: String,
+  description: String,
+  phone: String,
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
 });
-
 
 module.export = mongoose.model("User", userSchema);
