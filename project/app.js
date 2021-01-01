e052a727837b98298722e1aead9fdbc9540e358b
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-01-02 00:01:24 +0530

    Add Course

--- project/app.js
+++ project/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
 const authRoutes = require("./routes/auth-routes");
@@ -7,10 +8,14 @@ const adminroutes = require("./routes/admin-routes");
 const passportSetup = require("./config/passport-setup");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
+const coursesRoutes = require("./routes/courses-routes");
+var Course = require("./models/courses-model");
 
 const app = express();
 
 // set view engine
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 // set up session cookies
@@ -38,12 +43,60 @@ mongoose.connect(
 app.use("/auth", authRoutes);
 app.use("/profile", profileRoutes);
 app.use("/admin", adminroutes);
+app.use("/courses", coursesRoutes);
 
 // create home route
+app.get("/home", function (req, res) {
+  res.redirect("/");
+});
+
 app.get("/", (req, res) => {
   res.render("home", { user: req.user });
 });
 
+app.get("/find-course-by-topic", function (req, res) {
+  Course.find(function (err, courses) {
+    if (!courses) {
+      res.send("Sorry !!!");
+    } else {
+      res.render("find-course-by-topic", {
+        user: req.user,
+        courses_data: courses,
+      });
+    }
+  });
+});
+
+app.post("/find-course-by-topic", function (req, res) {
+  console.log(req.body.dropdown);
+  res.redirect("/find-course-by-topic");
+});
+
+app.get("/find-course-by-course-id", function (req, res) {
+  Course.find(function (err, courses) {
+    if (!courses) {
+      res.send("Sorry !!!");
+    } else {
+      res.render("find-course-by-course-id", {
+        user: req.user,
+        courses_data: courses,
+      });
+    }
+  });
+});
+
+app.get("/find-course-by-department", function (req, res) {
+  Course.find(function (err, courses) {
+    if (!courses) {
+      res.send("Sorry !!!");
+    } else {
+      res.render("find-course-by-department", {
+        user: req.user,
+        courses_data: courses,
+      });
+    }
+  });
+});
 app.listen(3000, () => {
   console.log("app now listening for requests on port 3000");
 });
