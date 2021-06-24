a660ec40053bd437d40df26dcf7b68a7dc79050a
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-06-24 16:39:01 +0530

    auth fixed

--- models/user.js
+++ models/user.js
@@ -1,10 +1,11 @@
-
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  email: { type: String, required: true },
+  name: { type: String, required: true },
   isAdmin: { type: Boolean, default: false },
-  outlookID: { type: String, required: true },
+  accessToken: { type: String, required: true, select: false },
+  outlookId: { type: String, required: true },
 });
 
 module.exports = mongoose.model("User", userSchema);
