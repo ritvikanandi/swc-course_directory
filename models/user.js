0131e0488db3a38617f713fe8c18c702d2eaeba2
Rahul Aggarwal <rahulaggarwal13500@gmail.com>
2021-06-27 22:49:31 +0530

    .

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   username: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false },
+  isAdmin: { type: Boolean, default: true },
   accessToken: { type: String, required: true, select: false },
   outlookId: { type: String, required: true },
 });
